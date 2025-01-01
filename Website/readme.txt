b94ed00959647e1612cd5bf30d3286d26ffe043d
Mcy61 <120719507+Mcy61@users.noreply.github.com>
2025-01-01 02:07:29 +0100

    changes
    first  i changed a few folder names, cause they were not appropriate.
    
    then i started with the website (i also changed the location of the folder, it's important yk), for the website i started changing the html (the important looking part), added a css, js file.
    
    The js file has already his functions (but empty), the css is almost empty. But the base design of the html is finished. There were a few problems with stroke-width, cause its not working, idk why, it should work, but its not, nothing to horrible to worry about, but it still annoys me.
    
    So in summarize the html is almost finished, and tomorrow i will set my sights to the js.file and implement the xy graph on the canvas.

---
+++ Website/readme.txt
@@ -0,0 +1,2 @@
+This website shows sensor data with the help of a graph. 
+The graph can be filtered trough the comboboxesand then be applied trough the apply button. 
---
+++ Website/web_code.js
@@ -0,0 +1,25 @@
+
+
+function setup(){
+
+}
+
+function apply_button(){ //applying the filter options on the graph
+
+}
+
+function update(){
+
+}
+
+function canvas_setting(){
+
+}
+
+function HTTP_GET(){
+
+}
+
+function HTTP_SET(){
+    
+}
