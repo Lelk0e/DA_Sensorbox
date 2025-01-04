3e65e61a63a37a8035953c76e3e9c8086b158725
Lelk0e <phillip.mayer@student.htl-rankweil.at>
2025-01-04 14:58:06 +0100

    Update namedMesh_AsyncWebserver_filesystem.ino
    Den Webserver erweitert mit dem Filesystem und den Code besser Lesbar zu machen

--- Code/namedMesh_AsyncWebserver_filesystem/namedMesh_AsyncWebserver_filesystem.ino
+++ Code/namedMesh_AsyncWebserver_filesystem/namedMesh_AsyncWebserver_filesystem.ino
@@ -14,13 +14,88 @@ String msgBME = "";
 String msgOzon = "";
 String msgPT = "";
 String msgInter = "";
+String hour;
+String minute;
+String second;
 
 AsyncWebServer server(80);
+
+void initMesh(){
+  mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION);
+  mesh.init("Sensorbox", "12345678", &userSched, 5555);
+  mesh.setRoot(true);
+  mesh.setContainsRoot(true);
+  Serial.println(mesh.getAPIP());
+  mesh.setName(nodeName);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections([]() {
+    Serial.printf("Changed connection\n");
+  });
+}
 
 void sendRoot();
 
 void sendRoot() {
   mesh.sendBroadcast(nodeName);
+}
+
+void receivedCallback(String &from, String &msg) {
+  Serial.printf("Received from=%s msg=%s\n", from.c_str(), msg.c_str());
+
+  if (from.equals("BME280")) {
+    msgBME = msg;
+    if (msgBME != ""){
+      File file = SD.open("/" + from + ".txt", FILE_APPEND);
+      if(file.print(msgBME)){
+        Serial.println("Data Printed To BME");
+      }else{
+        Serial.println("Printing Failed")
+      }
+      file.close();
+    }
+
+  } else if (from.equals("Ozon")) {
+    msgOzon = msg;
+    if (msgOzon != ""){
+      File file = SD.open("/" + from + ".txt", FILE_APPEND);
+      if(file.print(msgOzon)){
+        Serial.println("Data Printed To Ozon");
+      }else{
+        Serial.println("Printing Failed")
+      }
+      file.close();
+    }
+
+  } else if (from.equals("PT100")) {
+    msgPT = msg;
+    if (msgPT != ""){
+      File file = SD.open("/" + from + ".txt", FILE_APPEND);
+      if(file.print(msgPT)){
+        Serial.println("Data Printed To PT100");
+      }else{
+        Serial.println("Printing Failed")
+      }
+      file.close();
+    }
+
+  } else if (from.equals("Interchangable")) {
+    msgInter = msg;
+    if (msgInter != ""){
+      File file = SD.open("/" + from + ".txt", FILE_APPEND);
+      if(file.print(msgInter)){
+        Serial.println("Data Printed To Interchangable");
+      }else{
+        Serial.println("Printing Failed")
+      }
+      file.close();
+    }
+  }
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  sendRoot();
+  mesh.sendBroadcast(hour + ";" + minute + ";" + second);
 }
 
 void initSDCard(){
@@ -33,44 +108,39 @@ void initSDCard(){
   Serial.printf("Size: %lluMB\n", cardSize);
 }
 
-void receivedCallback(String &from, String &msg) {
-  Serial.printf("Received from=%s msg=%s\n", from.c_str(), msg.c_str());
-  if (from.equals("BME280")) {
-    msgBME = msg;
-  } else if (from.equals("Ozon")) {
-    msgOzon = msg;
-  } else if (from.equals("PT100")) {
-    msgPT = msg;
-  } else if (from.equals("Interchangable")) {
-    msgInter = msg;
+void serveFile(AsyncWebServerRequest *request, const char *filename) {
+  File file = SD.open(filename, FILE_READ);
+
+  if (!file || file.isDirectory()) {
+    request->send(404, "text/plain", "File not found or is a directory");
+    return;
   }
+
+  String fileContent;
+  while (file.available()) {
+    fileContent += char(file.read());
+  }
+  file.close();
+
+  request->send(200, "text/plain", fileContent);
 }
 
-void newConnectionCallback(uint32_t nodeId) {
-  sendRoot();
-}
+
 
 void setup() {
+
   Serial.begin(115200);
-  mesh.setDebugMsgTypes(ERROR | DEBUG | CONNECTION);
-  mesh.init("Sensorbox", "12345678", &userSched, 5555);
-  mesh.setRoot(true);
-  mesh.setContainsRoot(true);
-  Serial.println(mesh.getAPIP());
-  mesh.setName(nodeName);
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections([]() {
-    Serial.printf("Changed connection\n");
-  });
+
+  initMesh();
+
   initSDCard();
+
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SD, "/webpage/website.html", "text/html");
   });
+
   server.on("/set-time", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String hour;
-    String minute;
-    String second;
+    
     if (request->hasParam("hour") && request->hasParam("minute") && request->hasParam("second")) {
       hour = request->getParam("hour")->value();
       minute = request->getParam("minute")->value();
@@ -86,30 +156,27 @@ void setup() {
       request->send(400, "text/plain", "Missing time parameters");
     }
   });
+
+  server.on("/data/bme280", HTTP_GET, [](AsyncWebServerRequest *request) {
+    serveFile(request, "/BME280.txt");
+  });
+
+  server.on("/data/ozon", HTTP_GET, [](AsyncWebServerRequest *request) {
+    serveFile(request, "/Ozon.txt");
+  });
+
+  server.on("/data/pt100", HTTP_GET, [](AsyncWebServerRequest *request) {
+    serveFile(request, "/PT100.txt");
+  });
+
+  server.on("/data/interchangable", HTTP_GET, [](AsyncWebServerRequest *request) {
+    serveFile(request, "/Interchangable.txt");
+  });
+
   server.begin();
 
 }
 
 void loop() {
   mesh.update();
-  if (!msgBME.isEmpty()) {
-    Serial.println("BME280 Message: " + msgBME);  
-    Serial.println("____________________________________");
-    delay(1000);
-  }
-  if (!msgOzon.isEmpty()) {
-    Serial.println("Ozon Message: " + msgOzon);
-    Serial.println("____________________________________");
-    delay(1000);
-  }
-  if (!msgPT.isEmpty()) {
-    Serial.println("PT100 Message: " + msgPT);
-    Serial.println("____________________________________");
-    delay(1000);
-  }
-  if (!msgInter.isEmpty()) {
-    Serial.println("Interchangable Message: " + msgInter);
-    Serial.println("____________________________________");
-    delay(1000);
-  }
 }
